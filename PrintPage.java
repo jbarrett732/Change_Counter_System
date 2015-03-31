81b7708e6579e63077cbc262b16764fecef4a6ff
John Barrett <jbarrett732@gmail.com>
2015-03-30 20:18:19 -0400

    Added File Difference and change report functions

--- PrintPage.java
+++ PrintPage.java
@@ -15,9 +15,10 @@ import java.util.Iterator;
 public class PrintPage extends JFrame {
 
 	private File file1 = null;
+	private File file2 = null;
 
 	//CONSTRUCTS PAGE WINDOW AND ADDS CONTENT
-	public PrintPage(String op, String filePath) {
+	public PrintPage(String op, String filePath, String filePath2) {
 
 		//set up window
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -26,6 +27,7 @@ public class PrintPage extends JFrame {
 		setTitle("Print Page: " + op);
 
 		file1 = new File(filePath);
+		file2 = new File(filePath2);
 
 		//determine operation and execute
 		if(!file1.exists()) {
@@ -33,9 +35,9 @@ public class PrintPage extends JFrame {
 		} else if(op.equals("Listing")) { 
 			addContent(printListing());
 		} else if(op.equals("Change")) {
-			//TODO
+			//TODO - cycle 2
 		} else if(op.equals("Report")) {
-			//TODO
+			addContent(printReport());
 		} else {
 			//error, not valid operation
 		}
@@ -75,9 +77,8 @@ public class PrintPage extends JFrame {
 		//read file into array list and catch exceptions
 		ArrayList<String> l = null;
 		try {
-		 	l = readFile();
-		} 
-		catch(IOException ioe) {}
+		 	l = readFile(file1);
+		} catch(IOException ioe) {}
 
 		//turn list into on long string
 		String fileContent = "";
@@ -92,11 +93,39 @@ public class PrintPage extends JFrame {
  	}
 
 
+	public String printReport() {
+
+		//read file into array list and catch exceptions
+		ArrayList<String> added_lines   = null;
+		ArrayList<String> removed_lines = null;
+		added_lines   = differ(file1, file2);
+		removed_lines = differ(file2, file1);
+
+		//turn list into on long string
+		String fileContent = "New and Changed Lines:\n";
+		if(!added_lines.isEmpty()) {
+			Iterator<String> it1 = added_lines.iterator();
+			while(it1.hasNext()) {
+				fileContent += it1.next() + "\n";
+			}	
+		}
+		fileContent += "\nRemoved Lines:\n";
+		if(!removed_lines.isEmpty()) {
+			Iterator<String> it2 = removed_lines.iterator();
+			while(it2.hasNext()) {
+				fileContent += it2.next() + "\n";
+			}	
+		}
+
+		return fileContent;
+	}
+
+
 	//READS FILE INTO AN ARRAY BY LINE 
-	public ArrayList<String> readFile() throws IOException {
+	public ArrayList<String> readFile(File file) throws IOException {
 
 		//create input stream for reading file
-		FileInputStream fin = new FileInputStream(file1);
+		FileInputStream fin = new FileInputStream(file);
 		BufferedReader read = new BufferedReader(new InputStreamReader(fin));
 		
 		//read file into array list by line
@@ -108,12 +137,55 @@ public class PrintPage extends JFrame {
 		return list;
 	}
  
+
+	//RETURNS ALL NON-MATCHING LINES 
+	//use differ(old,new) for added/changed lines
+	//use differ(new,old) for removed lines
+	public ArrayList<String> differ(File prev, File curr) {
+
+		//create file for added/changes lines
+		ArrayList<String> added = new ArrayList<String>();
+
+		//iterate over the lines of newer file
+		Iterator<String> new_file = null; 
+		try {
+			new_file = readFile(curr).iterator();	
+		} catch(IOException ioe) {}
+
+		while(new_file.hasNext()) {
+			boolean match = false;
+			String new_line = new_file.next();
+
+			//iterate over the lines of older file
+			Iterator<String> old_file = null;
+			try {
+				old_file = readFile(prev).iterator();	
+			} catch(IOException ioe) {}
+
+			while(old_file.hasNext()) {
+				String old_line = old_file.next();	
+				if(old_line.equals(new_line)) {
+					//line is unchanged
+					match = true;
+					break;
+				}
+			}
+			
+			//add new line to return array
+			if(!match) {
+				added.add(new_line);	
+			}
+		}
+		return added;
+	}
+
+
 	
 	//DRIVER FOR CLASS TESTING
 	public static void main (String args[]) {
 
 		//supported operations so far "Listing", "Numbered Listing"
-		if(args.length == 2)
-			new PrintPage(args[0], args[1]);
+		if(args.length == 3)
+			new PrintPage(args[0], args[1], args[2]);
 	}
 }
